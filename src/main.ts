dd99813742c298d6ede227eb431e6339ff6bb0cc
Anthon Holmqvist <anthon.kendel@gmail.com>
2021-03-05 22:49:09 +0100

    feat: generate random petname

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import { adjectives } from "./data/adjectives";
 import { adverbs } from "./data/adverbs";
 import { names } from "./data/names";
 
-console.log(adverbs);
-console.log(adjectives);
-console.log(names);
+function randomValue(values: string[]): string {
+  Math.random();
+  const numberOfValues = values.length;
+  const index = Math.floor(Math.random() * numberOfValues);
+  return values[index];
+}
+
+const separator = "-";
+const value = [
+  randomValue(adverbs),
+  randomValue(adjectives),
+  randomValue(names),
+].join(separator);
+
+console.info(value);
